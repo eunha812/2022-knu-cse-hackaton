7abe07b212a6ca16fd7bcfeccb85f5ddd96483fc
thing-zoo <cathy4025@gmail.com>
2022-09-24 01:28:25 +0200

    add needer profile

--- sonsu/lib/pages/needer/needer_profile_page.dart
+++ sonsu/lib/pages/needer/needer_profile_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sonsu/utils/constants.dart';
 import 'package:sonsu/widgets/app_bar.dart';
+import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NeederProfilePage extends StatelessWidget {
   const NeederProfilePage({super.key});
@@ -10,10 +12,49 @@ class NeederProfilePage extends StatelessWidget {
     return Scaffold(
       appBar: buildAppbar('프로필', kPrimaryColor, true),
       body: SafeArea(
+          child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 100),
           child: Column(
-        children: [
-          //
-        ],
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              CircleAvatar(
+                maxRadius: 60,
+                backgroundColor: Colors.transparent,
+                child: ClipOval(
+                  child: Image.asset('assets/images/needer.png'),
+                ),
+              ),
+              Text(
+                '${Get.arguments['name']}',
+                style: TextStyle(
+                  fontSize: 25.sp,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '${Get.arguments['age']}',
+                style: TextStyle(
+                  fontSize: 16.sp,
+                ),
+              ),
+              Text(
+                '${Get.arguments['gender']}',
+                style: TextStyle(
+                  fontSize: 16.sp,
+                ),
+              ),
+              const Text(
+                '신고횟수 0회',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: kMainRed,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
+        ),
       )),
     );
   }
