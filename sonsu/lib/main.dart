302a91d5ea08c7263941bdd448fed0a55645213d
Seungsin <8050aa@naver.com>
2022-09-24 11:14:42 +0900

    test

--- sonsu/lib/main.dart
+++ sonsu/lib/main.dart
@@ -1,28 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/instance_manager.dart';
 import 'package:get/route_manager.dart';
-import 'package:sonsu/controllers/notification_controller.dart';
-import 'package:sonsu/pages/helper/helper_main/needer_detail.dart';
-import 'package:sonsu/pages/needer/helping_page.dart';
-import 'package:sonsu/pages/needer/match_try_page.dart';
 import 'package:sonsu/pages/helper/helper_main/alarm_list.dart';
-import 'package:sonsu/pages/needer/needer_home_page.dart';
-import 'package:sonsu/pages/needer/match_complete_page.dart';
+import 'package:sonsu/pages/helpee/helpee_home_page.dart';
+import 'package:sonsu/pages/helpee/match_complete_page.dart';
 import 'package:sonsu/pages/home_page.dart';
-import 'package:sonsu/pages/needer/needer_profile_page.dart';
-import 'package:sonsu/pages/helper/helper_main/alarm_list.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:sonsu/pages/sign_up_in/sign_in_page.dart';
-import 'package:sonsu/pages/sign_up_in/sign_up_option_page.dart';
-import 'package:sonsu/pages/sign_up_in/sign_up_page.dart';
-import 'firebase_options.dart';
+import 'package:sonsu/pages/splash_page.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
 }
 
@@ -38,35 +23,53 @@ class MyApp extends StatelessWidget {
         return GetMaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Son-Su',
-          initialRoute: '/sign-up-option',
+          initialRoute: '/',
           getPages: [
-            GetPage(name: '/', page: () => const HomePage()),
+            GetPage(name: '/', page: () => const SplashtoHome()),
             GetPage(
                 name: '/home',
                 page: () => const HomePage(),
                 transition: Transition.fadeIn),
-            GetPage(name: '/alarm', page: () => const AlarmList()),
-            GetPage(name: '/needer-home', page: () => const NeederHomePage()),
-            GetPage(name: '/match-try', page: () => const MatchTryPage()),
+            GetPage(name: '/splash', page: () => const SplashPage()),
+            GetPage(name: '/alarm', page: () => AlarmList()),
+            GetPage(name: '/helpee-home', page: () => const HelpeeHomePage()),
             GetPage(
                 name: '/match-complete', page: () => const MatchCompletePage()),
-            GetPage(name: '/helping', page: () => const HelpingPage()),
-            // GetPage(name: '/helping-history', page: () => const HelpingHistoryPage()),
-            GetPage(
-                name: '/needer-profile', page: () => const NeederProfilePage()),
-            GetPage(name: '/alarm_list', page: () => const AlarmList()),
-            GetPage(name: '/sign-in', page: () => const SignInPage()),
-            GetPage(name: '/sign-up', page: () => const SignUpPage()),
-            GetPage(
-                name: '/sign-up-option', page: () => const SignUpOptionPage()),
-            GetPage(name: '/needer_detail', page: () => const NeederDetail()),
+            // GetPage(name: '/SignIn', page: () => const SignInPage()),
           ],
-          initialBinding: BindingsBuilder.put(() => NotificationController(),
-              permanent: true),
           theme: ThemeData(
             fontFamily: 'Noto_Sans_KR',
           ),
         );
+      },
+    );
+  }
+}
+
+class SplashtoHome extends StatelessWidget {
+  const SplashtoHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Init.instance.initialize(),
+      builder: (context, AsyncSnapshot snapshot) {
+        // Show splash screen while waiting for app resources to load:
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: SplashPage(),
+          );
+        } else {
+          // Loading is done, return the app:
+
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Son-Su',
+            theme: ThemeData(),
+            home: const HomePage(),
+          );
+        }
       },
     );
   }
--- sonsu/lib/pages/helper/helper_main/alarm_list.dart
+++ sonsu/lib/pages/helper/helper_main/alarm_list.dart
@@ -1,99 +1,18 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:sonsu/utils/constants.dart';
-import 'package:sonsu/widgets/app_bar.dart';
-import 'package:sonsu/widgets/rounded_button.dart';
 
 class AlarmList extends StatefulWidget {
-  const AlarmList({Key? key}) : super(key: key);
+  AlarmList({Key? key}) : super(key: key);
 
   @override
   State<AlarmList> createState() => _AlarmListState();
 }
 
 class _AlarmListState extends State<AlarmList> {
-  final emergency_List = [
-    {"title": "76세 / 거동 불편 / 여성", "time": "9"},
-    {"title": "65세 / 휠체어사용 / 남성", "time": "9"},
-    {"title": "45세 / 시각 장애 / 여성", "time": "8"},
-    {"title": "62세 / 거동 불편 / 남성", "time": "7"},
-    {"title": "73세 / 치매 / 남성", "time": "5"},
-    {"title": "76세 / 허리디스크 / 여성", "time": "3"},
-    {"title": "60세 / 언어 장애 / 여성", "time": "2"},
-    {"title": "76세 / 거동 불편 / 남성", "time": "1"},
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppbar("긴급 요청", kMainYellow, false),
-      body: ListView.separated(
-        padding: EdgeInsets.all(0),
-        itemCount: 8,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-              height: 70,
-              padding: EdgeInsets.fromLTRB(17, 3, 10, 3),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text.rich(
-                              TextSpan(
-                                text: emergency_List[index]
-                                    ["title"]!, // default text style
-                                style: TextStyle(
-                                    fontSize: 15, fontWeight: FontWeight.w900),
-                                children: <TextSpan>[
-                                  TextSpan(
-                                      text: '이 도움을 요청합니다!',
-                                      style: TextStyle(
-                                          color: Colors.black38,
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.w400)),
-                                ],
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(
-                                    "요청 종료까지 " +
-                                        emergency_List[index]["time"]! +
-                                        "분 남았습니다",
-                                    style: TextStyle(
-                                        fontSize: 11.0, color: kMainRed))
-                              ],
-                            )
-                          ]),
-                    ),
-                    InkWell(
-                      splashColor: Colors.transparent,
-                      child: Container(
-                        padding: EdgeInsets.fromLTRB(20, 0, 10, 0),
-                        height: 30,
-                        child: Image.asset(
-                          'assets/images/yellowhand.png',
-                        ),
-                      ),
-                      onTap: () {
-                        Get.toNamed('/needer_detail');
-                      },
-                    )
-                  ]));
-        },
-        separatorBuilder: (BuildContext ctx, int idx) {
-          return Divider();
-        },
-      ),
+    return Container(
+      color: kMainLightGreen,
     );
   }
 }
